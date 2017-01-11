270f0ebb7219f4b50459ee5069282e7459ac1451
danlurie <danjlurie@gmail.com>
2017-01-11 15:19:51 -0800

    Comitting previous changes which I made long ago and don't remember.

--- bic_dcm2nii.py
+++ bic_dcm2nii.py
@@ -18,11 +18,17 @@ Notes:
    
 dicom_dir: The path to a directory containing your subject folders.
            Each subject folder should contain the following:
-	   - At least one anatomical scan (i.e. a folder with 'T1MPRAGE' somewhere in it's name).
-	   - At least one functional scan (i.e. a folder with 'EPI' somewhere in it's name).
+	   - At least one anatomical scan folder.
+	   - At least one functional scan folder.
 
 nifti_dir: A directory where you would like NIfTI files written.
 	   If this directory does not exist, one will be created.
+
+anat_name: A string common to all anatomical scan folder names.
+           Example: Given folders MPRAGE_1 & MPRAGE_2, anat_name = MPRAGE
+
+func_name: A string common to all functional scan folder names.
+           Example: Given folders Rest1 & Rest2, func_name = Rest
 """
 
 import os
@@ -31,42 +37,55 @@ import subprocess
 
 dicom_dir = sys.argv[1]
 nifti_dir = sys.argv[2]
+anat_name = sys.argv[3]
+func_name = sys.argv[4]
 
 subject_list = os.listdir(dicom_dir)
 
 for subject in subject_list:
-	# Get the path to the raw data for this subject.
-	subject_raw_path = '/'.join([dicom_dir, subject])
-	# Set the directory where NIfTIs will be written for this subject.
-	subject_nifti_path = '/'.join([nifti_dir, subject])
-	# Set paths for where we will store anatomical and functional NIfTIs.
-	func_path = '/'.join([subject_nifti_path, 'func'])
-	anat_path = '/'.join([subject_nifti_path, 'anat'])
-	# Create directories for anatomical and functional NIfTIs.
-	os.makedirs(func_path)
-	os.makedirs(anat_path)
-	# Get a list of all scans we have for this subject.
-	scan_list = os.listdir(subject_raw_path)
-	# Get a list of all MPRAGE scans.
-	mprage_list = filter(lambda x: 'T1MPRAGE' in x, scan_list)
-	# Get a list of all EPI scans.
-	epi_list = filter(lambda x: 'EPI' in x, scan_list)
-	# Convert MPRAGE scans
-	for mprage_scan in mprage_list:
-		# Get the path of the DICOMs for this scan.
-		mprage_raw_path = '/'.join([subject_raw_path, mprage_scan])
-		# Create a folder in ../anat for this scan.
-		mprage_nifti_dir = '/'.join([anat_path, mprage_scan])
-		os.makedirs(mprage_nifti_dir)
-		# Call dcm2nii to do the actual conversion.
-		subprocess.check_call(['dcm2nii', '-o', mprage_nifti_dir, mprage_raw_path])
-	# Convert EPI scans
-	for epi_scan in epi_list:
-		# Get the path of the DICOMs for this scan.
-		epi_raw_path = '/'.join([subject_raw_path, epi_scan])
-		# Create a folder in ../anat for this scan.
-		epi_nifti_dir = '/'.join([func_path, epi_scan])
-		os.makedirs(epi_nifti_dir)
-		# Call dcm2nii to do the actual conversion.
-		subprocess.check_call(['dcm2nii', '-o', epi_nifti_dir, epi_raw_path])
-	
+    # Get the path to the raw data for this subject.
+    subject_raw_path = '/'.join([dicom_dir, subject])
+    # Set the directory where NIfTIs will be written for this subject.
+    subject_nifti_path = '/'.join([nifti_dir, subject])
+    # Set paths for where we will store anatomical and functional NIfTIs.
+    func_path = '/'.join([subject_nifti_path, 'func'])
+    anat_path = '/'.join([subject_nifti_path, 'anat'])
+    # Create directories for anatomical and functional NIfTIs.
+    if os.path.isdir(func_path) == False:
+        os.makedirs(func_path)
+    else:
+        pass
+    if os.path.isdir(anat_path) == False:
+        os.makedirs(anat_path)
+    else:
+        pass
+    # Get a list of all scans we have for this subject.
+    scan_list = os.listdir(subject_raw_path)
+    # Get a list of all MPRAGE scans.
+    mprage_list = filter(lambda x: anat_name in x, scan_list)
+    # Get a list of all EPI scans.
+    epi_list = filter(lambda x: func_name in x, scan_list)
+    # Convert MPRAGE scans
+    for mprage_scan in mprage_list:
+        # Get the path of the DICOMs for this scan.
+        mprage_raw_path = '/'.join([subject_raw_path, mprage_scan])
+        # Create a folder in ../anat for this scan.
+        mprage_nifti_dir = '/'.join([anat_path, mprage_scan])
+        if os.path.isdir(mprage_nifti_dir) == False:
+            os.makedirs(mprage_nifti_dir)
+            # Call dcm2nii to do the actual conversion.
+            subprocess.check_call(['dcm2nii', '-o', mprage_nifti_dir, mprage_raw_path])
+        else:
+            pass
+    # Convert EPI scans
+    for epi_scan in epi_list:
+        # Get the path of the DICOMs for this scan.
+        epi_raw_path = '/'.join([subject_raw_path, epi_scan])
+        # Create a folder in ../anat for this scan.
+        epi_nifti_dir = '/'.join([func_path, epi_scan])
+        if os.path.isdir(epi_nifti_dir) == False:
+            os.makedirs(epi_nifti_dir)
+            # Call dcm2nii to do the actual conversion.
+            subprocess.check_call(['dcm2nii', '-o', epi_nifti_dir, epi_raw_path])
+        else:
+            pass	
